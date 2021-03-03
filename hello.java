ef8766dd812f59a0809c53dd153a06f7ce78cea3
dig8teacher <jgill@sd42.ca>
2021-03-02 19:35:49 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@ class hello
 {
   public static void main(String[] args)
   {
+    int test = 10;
     System.out.println("hello maggie");
     System.out.println("we are live from jenkins");
     System.out.println("this is how we do continuous delivery or continuous integration");
+    if (test == 10) {
+      System.out.println("this is a 10 out of 10");
+    }
     }
  }
