705dbb0295df103c162e31d561545ad8cdc40254
dig8teacher <jgill@sd42.ca>
2021-03-04 19:49:02 -0800

    Add files via upload

---
+++ variabletwo.java
@@ -0,0 +1,9 @@
+class variabletwo {
+	public static void main(String args[]) 
+	{
+	int number1 = 7;
+	int number2 = 7;
+	System.out.println("the answer when you subtract " + number1 + " and " + number2 + " is ");
+	System.out.println(number1-number2);
+	}
+}
